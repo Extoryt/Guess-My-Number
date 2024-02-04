66d02eabdd3b7997ff9c1c506cbca467e9d86d61
Extoryt <96318106+Extoryt@users.noreply.github.com>
2024-02-04 11:45:30 +0100

    Project

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
-document.querySelector('.check').addEventListener('click', function () {
+const Game = function () {
   const guess = Number(document.querySelector('.guess').value);
 
   //When there is no input
@@ -40,15 +40,18 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+};
 
-  const resetGame = function () {
-    score = 20;
-    document.querySelector('body').style.backgroundColor = '#222';
-    document.querySelector('.number').style.width = '15rem';
-    document.querySelector('.number').textContent = '?';
-    document.querySelector('.message').textContent = 'Start guessing...';
-    document.querySelector('.guess').value = '';
-    document.querySelector('.score').textContent = score;
-  };
-  document.querySelector('.again').addEventListener('click', resetGame);
-});
+document.querySelector('.check').addEventListener('click', Game);
+
+const resetGame = function () {
+  score = 20;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = score;
+};
+
+document.querySelector('.again').addEventListener('click', resetGame);
